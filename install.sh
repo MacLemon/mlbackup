9ea4a2a7a8936da545c85af9254e7ebcc7b309c6
pepi <pepi@1241b21e-4b16-0410-9ea6-eb4be85eeab3>
2006-07-26 21:03:11 +0000

    Corrected the README file to reflect the current status of mlbackup. Corrected install.sh to actually correctly install mlbackup and the rest of the files to their appropriate places.

--- install.sh
+++ install.sh
@@ -8,10 +8,9 @@
 # Status: ALPHA
 
 MLbackupInstallerVersion="0.0.1"
-MLbackupInstallerBuild="1"
-MLbackupBuild=46
+MLbackupBuild=26
 
-# LastChange: 2006.05.01.
+# LastChange: 2006.07.26.
 ###############################################################################
 # Must be run as root
 
@@ -23,46 +22,39 @@ MLlnFlags="-vi"
 MLrootUID=0
 if [ "$UID" -ne "$MLrootUID" ]
 	then # We are not root
-		MLweAreRoot=0
-		echo $0 must be run as root.
+		echo Sorry, $(basename $0) must be run as root.
 
 		# install.sh should check if the user is able to provide a password that can be used.
 		# Member of group admin or wheel on Mac OS X should be enough, maybe check the sudoers file as well.
-		sudo $0
-		
-		
+		exit 1	
 	else # Yes, we are root.
-		MLweAreRoot=1
 	echo
 	echo Creating directories
 	# Creating the directory to keep the scripts
 	mkdir -p $MLbinDir	
 	
-	# Creating the directory to keep the configs
+	# Creating the directory to keep the sample config and globalexclusions
 	mkdir -p $MLconfigDir
 	
 	echo
 	echo copying files
 	# copying the files
-	cp $MLcpFlags rsyncautolocator.sh $MLbinDir/
 	cp $MLcpFlags growler.sh $MLbinDir/
-	cp $MLcpFlags mlbackup-v$MLbackupBuild $MLbinDir/
-	ln -s $MLlnFlags $MLbinDir/mlbackup-v$MLbackupBuild $MLbinDir/mlbackup
+	cp $MLcpFlags mlbackup $MLbinDir/
 
-	cp $MLcpFlags globalexclusions-v$MLbackupBuild $MLconfigDir/
-	ln -s $MLlnFlags $MLconfigDir/globalexclusions-v$MLbackupBuild $MLconfigDir/globalexclusions
+	cp $MLcpFlags globalexclusions $MLconfigDir/
 	cp $MLcpFlags demo.mlbackupconf.sample $MLconfigDir/
 
 	# Setting correct Ownership of files
-	chown root:wheel $MLbinDir/mlbackup-v$MLbackupBuild $MLbinDir/growler.sh $MLbinDir/rsyncautolocator.sh $MLconfigDir/globalexclusions $MLconfigDir
+	chown root:wheel $MLbinDir/mlbackup $MLbinDir/growler.sh $MLconfigDir/globalexclusions $MLconfigDir
 	
 	echo
 	echo Setting correct privileges and file modes
 	# Setting correct file modes
-	chmod 755 $MLbinDir/mlbackup-v$MLbackupBuild $MLbinDir/growler.sh $MLbinDir/rsyncautolocator.sh $MLconfigDir
+	chmod 755 $MLbinDir/mlbackup $MLbinDir/growler.sh $MLconfigDir
 	chmod 644 $MLconfigDir/globalexclusions $MLconfigDir/demo.mlbackupconf.sample
 
-	echo Done installing MacLemon backup scripts. See the README or http://maclemon.at/ for more info
+	echo Done installing the MacLemon backup scripts. See the README for more info
 fi
-
+return 0
 #EOF
