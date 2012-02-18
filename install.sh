a5553be8d54154990f4fed22ed723fe81329f7bc
Pepi Zawodsky <github.com@maclemon.at>
2012-02-18 20:49:49 +0100

    Corrected a bug that would try to fix postfix on 10.7 and fail. This part is now only run on 10.[456].

--- install.sh
+++ install.sh
@@ -71,11 +71,14 @@ if [ "$UID" -ne "$MLrootUID" ]
 	chmod 755 $MLbinDir/mlbackup $MLconfigDir
 	chmod 644 $MLconfigDir/globalexclusions $MLconfigDir/demo.mlbackupconf.sample
 	
-	if [ ! $(sw_vers | grep "Server") ]
+	if [ ! $(sw_vers | grep -q "Server") ]
 	then
-		echo
-		echo Fixing the postifx queue system on Mac OS X client
-		/etc/postfix/post-install create-missing
+        if [ $(sw_vers | grep -q "10.[456]") ]
+        then
+            echo
+            echo Fixing the postifx queue system on Mac OS X client
+            /etc/postfix/post-install create-missing
+        fi
 	else
 		echo
 		echo This is Mac OS X Server, not touching the postfix system.
