f40de241057abfd6b92e50dba94b13dc6573a56f
Pepi Zawodsky <github.com@maclemon.at>
2012-02-19 22:20:35 +0100

    Fixed a small bug that would fail fixing postfix on non ServerVersion of Mac OS X 10.7.

--- fix-postfix-queue.sh
+++ fix-postfix-queue.sh
@@ -2,14 +2,16 @@
 # fixes the postfix queue on Mac OS X Client
 # Never touches postfix on Mac OS X Server.
 
-
-	if [ ! $(sw_vers | grep "Server") ]
-	then
-		echo
-		echo Fixing the postifx queue system on Mac OS X client
-		/etc/postfix/post-install create-missing
-	else
-		echo
-		echo This is Mac OS X Server, not touching the postfix system.
-	fi
+if [ ! $(sw_vers | grep -q "Server") ]
+then
+    if [ $(sw_vers | grep -q "10.[456]") ]
+    then
+        echo
+        echo Fixing the postifx queue system on Mac OS X client
+        /etc/postfix/post-install create-missing
+    fi
+else
+     echo
+    echo This is Mac OS X Server, not touching the postfix system.
+fi
 #EOF
